d1025d6284f914675b0d789a3a04a5c3659d1e60
AltSumpreme <reubenphilip52009@gmail.com>
2025-01-03 22:20:28 +0530

    linter errors and bugs fixed --reuben

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+import globals from "globals";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    ignores: [
+      "node_modules",
+      ".eslintignore",
+      ".gitignore",
+      "pre-commit",
+      ".prettierignore",
+      ".env",
+    ],
+    files: ["**/*.{js,mjs,cjs,ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...prettierConfig.rules,
+      "prettier/prettier": [
+        "error",
+        {
+          semi: true,
+          singleQuote: false,
+          trailingComma: "es5",
+          printWidth: 80,
+          tabWidth: 2,
+        },
+      ],
+    },
+  },
+];
