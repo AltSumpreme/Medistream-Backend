8edb9c4a3cea12ade538b6f9c0e7c55dd2948964
Abhinav Anil <abhinav.anil.2k5@gmail.com>
2025-03-27 00:11:50 +0530

    feat: fix ci/cd and few changes

--- src/schemas/appointment.ts
+++ src/schemas/appointment.ts
@@ -1,5 +1,6 @@
 import { z } from "@hono/zod-openapi";
-import { patientSchema, doctorSchema } from "./patient.js";
+import { patientSchema } from "./patient.js";
+import { doctorSchema } from "./doctor.js";
 export const appointmentSchema = z.object({
   id: z.string().cuid().openapi({ example: "cm5mccbjk00002063ebefgxjp" }),
   date: z.date().openapi({ example: "2022-01-01T00:00:00.000Z" }),
--- src/schemas/patient.ts
+++ src/schemas/patient.ts
@@ -2,7 +2,13 @@ import { z } from "@hono/zod-openapi";
 import { appointmentSchema } from "./appointment.js";
 import { userSchema } from "./user.js";
 import { medicalRecordSchema } from "./medicalRecord.js";
-export const patientSchema = z.object({
+export const patientSchema: z.ZodType<{
+  id: string;
+  birthDate: Date;
+  user: z.infer<typeof userSchema>[];
+  medicalRecords: z.infer<typeof medicalRecordSchema>[];
+  appointments: z.infer<typeof appointmentSchema>[];
+}> = z.object({
   id: z.string().cuid().openapi({ example: "cm5mccbjk00002063ebefgxjp" }),
   birthDate: z.date().openapi({ example: "2021-09-01" }),
   user: z.array(userSchema),
